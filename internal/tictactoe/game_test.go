67a0914649d310b0572fc1848a3ae74a1c53d813
A Software Guy Doing Hardware <81204917+asoftwareguydoinghardware@users.noreply.github.com>
2026-05-29 14:34:15 -0700

    TDD: HandleValidMoveFromPlayer() calls ReportBadMoveSelection() if move was "" (empty string).

--- internal/tictactoe/game_test.go
+++ internal/tictactoe/game_test.go
@@ -128,6 +128,7 @@ func TestHandleValidMoveFromPlayerCallsReportBadMoveSelectionForGoodMoveApropria
 		{"1", true},
 		{"5", true},
 		{"9", false},
+		{"", false},
 	}
 
 	for _, tc := range testCases {
